1c0f224c8a52d4e64bbb76652257f61c5e562f54
Davide Spataro <davide90.spataro@gmail.com>
2019-12-21 16:39:03 +0100

    completes the draft for the two string anagram problem. Add UT for the histogram solution

--- test/two_string_anagram/test_two_string_anagram.cpp
+++ test/two_string_anagram/test_two_string_anagram.cpp
@@ -1,30 +1,54 @@
 #include <gtest/gtest.h>
 #include <bits/stdc++.h>
 
+#include "two_string_anagram_brute_force.cpp"
 #include "two_string_anagram_sorting.cpp"
+#include "two_string_anagram_histogram.cpp"
 
 using std::string;
 
+
+
+TEST(two_string_anagram_sorting, given_two_empty_string_result_should_be_zero)
+{
+	string a = "";
+	string b (a);
+	constexpr int expected_value = 0;
+	EXPECT_EQ(0, solution_sorting(a,b));
+	EXPECT_EQ(0, solution_brute_force(a,b));
+	EXPECT_EQ(0, solution_histogram(a,b));
+}
 
 TEST(two_string_anagram_sorting, given_two_equal_string_result_should_be_zero)
 {
 	string a = "aaaa";
 	string b (a);
+	constexpr int expected_value = 0;
 	EXPECT_EQ(0, solution_sorting(a,b));
+	EXPECT_EQ(0, solution_brute_force(a,b));
+	EXPECT_EQ(0, solution_histogram(a,b));
 }
 
 TEST(two_string_anagram_sorting, given_two_string_one_reverse_than_the_other_result_should_be_zero)
 {
 	string a = "tear";
-	string b (a.rbegin(), a.rend());	
-	EXPECT_EQ(0, solution_sorting(a,b));
+	string b (a.rbegin(), a.rend());
+	constexpr int expected_value = 0;
+
+	EXPECT_EQ(expected_value, solution_sorting(a,b));
+	EXPECT_EQ(expected_value, solution_brute_force(a,b));
+	EXPECT_EQ(expected_value, solution_histogram(a,b));
 }
 
 TEST(two_string_anagram_sorting, given_two_strings_differet_length_result_should_be_minus1)
 {
 	string a = "aaaa";
 	string b ("bbbbbbbbb");
-	EXPECT_EQ(-1, solution_sorting(a,b));
+	constexpr int expected_value = -1;
+
+	EXPECT_EQ(expected_value, solution_sorting(a,b));
+	EXPECT_EQ(expected_value, solution_brute_force(a,b));
+	EXPECT_EQ(expected_value, solution_histogram(a,b));
 }
 
 
