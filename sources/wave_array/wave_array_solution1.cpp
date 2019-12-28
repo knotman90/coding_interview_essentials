f4f6155fbcdae2b6da1c55c83cd6ac8b17d390d9
Davide Spataro <davide90.spataro@gmail.com>
2019-12-28 13:23:24 +0100

    fix build for wave solution

--- sources/wave_array/wave_array_solution1.cpp
+++ sources/wave_array/wave_array_solution1.cpp
@@ -1,11 +1,11 @@
-vector<int> Solution::wave(vector<int> &A) 
+std::vector<int> wave(std::vector<int> &A) 
 {
-    vector<int> B(A);
-    sort(begin(B), end(B));
+    std::vector<int> B(A);
+    std::sort(begin(B), end(B));
     auto it = B.begin();
     for(auto it = B.begin(); it+1 < B.end() ; it+=2)
     {
-        swap(*it, *(it+1));
+        std::swap(*it, *(it+1));
     }
     return B;
 }
