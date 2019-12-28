f615ce81edd0bd51c4109572192dbcba4516b04f
Davide Spataro <davide90.spataro@gmail.com>
2019-12-28 13:07:19 +0100

    add draft for wave array problem

---
+++ test/wave_array/test_wave_array.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <bits/stdc++.h>
+
+#include "wave_array_solution1.cpp"
+#include "wave_array_solution2.cpp"
+
+using std::string;
+
+
+
+TEST(wave_array, test1)
+{
+	
+	EXPECT_EQ(0,1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
+
