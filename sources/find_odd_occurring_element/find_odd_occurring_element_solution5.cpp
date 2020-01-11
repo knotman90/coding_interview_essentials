900230edb55182550f0aa3191231ff6a48b2c986
Davide Spataro <davide90.spataro@gmail.com>
2020-01-11 18:44:20 +0100

    add draft of the find_odd_occurring_element problem

---
+++ sources/find_odd_occurring_element/find_odd_occurring_element_solution5.cpp
@@ -0,0 +1,5 @@
+int odd_appearing_element_final_std(const std::vector<int> &A)
+{
+  return std::accumulate(begin(A), end(A), 0,
+                         [](const int acc, const int x) { return acc ^ x; });
+}
