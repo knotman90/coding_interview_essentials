68d2dd30f7b831d19bc5625679b410bab055cfb7
Davide Spataro <davide90.spataro@gmail.com>
2020-03-29 18:48:37 +0200

    solution 2 to the verify BST property problem

---
+++ sources/verify_BST/verify_BST_solution2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int tree_min(TreeNode *root)
+{
+  if (!root)
+    return std::numeric_limits<int>::max();
+  return std::min({root->val, tree_min(root->left), tree_min(root->right)});
+}
+
+int tree_max(TreeNode *root)
+{
+  if (!root)
+    return std::numeric_limits<int>::min();
+  return std::max({root->val, tree_max(root->left), tree_max(root->right)});
+}
+bool isValidBST_min_max(TreeNode *root)
+{
+  if (!root)
+    return true;
+
+  bool left_ok = true;
+  if (root->left)
+    left_ok = tree_max(root->left) <= root->val;
+  bool right_ok = true;
+  if (root->right)
+    right_ok = tree_min(root->right) > root->val;
+
+  return (left_ok && right_ok) &&
+         (isValidBST_min_max(root->left) && isValidBST_min_max(root->right));
+}
