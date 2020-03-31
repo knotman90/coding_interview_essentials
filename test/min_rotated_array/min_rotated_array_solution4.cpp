766ebac3fccc3201c66b1d5b830370e00d2e10df
Davide Spataro <davide90.spataro@gmail.com>
2020-03-31 15:41:00 +0200

    Beta version

--- test/min_rotated_array/min_rotated_array_solution4.cpp
+++
@@ -1,1 +0,0 @@
-../../sources/min_rotated_array/min_rotated_array_solution4.cpp
--- test/min_rotated_array/test_min_rotated_array.cpp
+++ test/min_rotated_array/test_min_rotated_array.cpp
@@ -10,6 +10,7 @@ using std::endl;
 
 #include "min_rotated_array_solution1.cpp"
 #include "min_rotated_array_solution2.cpp"
+#include "min_rotated_array_solution3.cpp"
 
 
 
@@ -19,6 +20,7 @@ TEST(min_rotated_array, test_one_element_vector)
 	const std::vector<int> V{1};
 	EXPECT_EQ(1,min_rotated_array_brute_force(V));
 	EXPECT_EQ(1,min_rotated_array_brute_force_1(V));
+	EXPECT_EQ(1,min_rotated_array_brute_force_log(V));
 }
 
 TEST(min_rotated_array, test_no_rotation_vector)
@@ -26,27 +28,30 @@ TEST(min_rotated_array, test_no_rotation_vector)
 	const std::vector<int> V{1,2,3,4,5,6,7,8,9,10};
 	EXPECT_EQ(1,min_rotated_array_brute_force(V));
 	EXPECT_EQ(1,min_rotated_array_brute_force_1(V));
+	EXPECT_EQ(1,min_rotated_array_brute_force_log(V));
 }
 
 TEST(min_rotated_array, test_all_rotations)
 {
 	const std::vector<int> V_original{1,2,3,4,5,6,7,8,9,10};
-	for(int i = 0 ; i < V_original.size() ; i++){
+	for(size_t i = 0 ; i < V_original.size() ; i++){
 		std::vector<int> V(V_original);
 		std::rotate(begin(V), begin(V)+i, end(V));
 		EXPECT_EQ(1,min_rotated_array_brute_force(V));
 		EXPECT_EQ(1,min_rotated_array_brute_force_1(V));
+		EXPECT_EQ(1,min_rotated_array_brute_force_log(V));
 	}
 }
 
 TEST(min_rotated_array, test_all_rotations_negative)
 {
-	const std::vector<int> V_original{-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
-	for(int i = 0 ; i < V_original.size() ; i++){
+	const std::vector<int> V_original{-10,-9,-8,-7,-6,-5,-4,-3,-2,-1};
+	for(size_t i = 0 ; i < 2 ; i++){
 		std::vector<int> V(V_original);
 		std::rotate(begin(V), begin(V)+i, end(V));
 		EXPECT_EQ(-10,min_rotated_array_brute_force(V));
 		EXPECT_EQ(-10,min_rotated_array_brute_force_1(V));
+		EXPECT_EQ(-10,min_rotated_array_brute_force_log(V));
 	}
 }
 
