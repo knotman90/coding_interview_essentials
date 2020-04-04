e3b3ad91ccc320737c5cca70534249942aa5d360
Davide Spataro <davide90.spataro@gmail.com>
2020-04-04 13:35:00 +0200

    add list of listings. add c++ solution ro te ranodm point generation in a circle

---
+++ sources/random_points_in_circle/random_points_in_circle_solution1.cpp
@@ -0,0 +1,10 @@
+auto generate_random_point_in_circle()
+{
+  static std::uniform_real_distribution<double> dist_radius(0, 1);
+  static std::uniform_real_distribution<double> dist_angle(0, 2 * M_PI);
+  const auto r     = radius * sqrt(dist_radius(rnd));
+  const auto theta = dist_angle(rnd);
+  const auto x     = r * cos(theta);
+  const auto y     = r * sin(theta);
+  return std::make_pair{x + x_center, y + y_center};
+}
