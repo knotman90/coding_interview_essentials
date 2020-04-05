36eea11a573e0d53d6dcedab2e44a8cf85e10071
Davide Spataro <davide90.spataro@gmail.com>
2020-04-05 22:56:02 +0200

    beta version of the buy and sell stock problem 2

--- sources/buy_sell_stocks/buy_sell_stocks_2/buy_sell_stocks_2_solution1.cpp
+++ sources/buy_sell_stocks/buy_sell_stocks_2/buy_sell_stocks_2_solution1.cpp
@@ -1,12 +1,24 @@
-int buy_sell_stocks_multiple_transactions(std::vector<int> &P)
+int buy_sell_stocks_multiple_transactions_exp_helper(const std::vector<int> &P,
+                                                     const int start)
 {
-  if (P.size() <= 0)
-    return 0;
   int ans = 0;
-  for (int i = 0; i < P.size() - 1; i++)
+  for (int buy_day = start; buy_day < P.size() - 1; buy_day++)
   {
-    if (P[i] < P[i + 1])
-      ans += (P[i + 1] - P[i]);
+    for (int sell_day = buy_day + 1; sell_day < P.size(); sell_day++)
+    {
+      if (P[buy_day] < P[sell_day]) // pointless to sell otherwise
+      {
+        const int selling_profit = P[sell_day] - P[buy_day];
+        const int profit_rest_transactions =
+            buy_sell_stocks_multiple_transactions_exp_helper(P, sell_day + 1);
+        ans = std::max(ans, selling_profit + profit_rest_transactions);
+      }
+    }
   }
   return ans;
+}
+
+int buy_sell_stocks_multiple_transactions_exp(const std::vector<int> &P)
+{
+  return buy_sell_stocks_multiple_transactions_exp_helper(P, 0);
 }
--- test/buy_sell_stocks_2/test_buy_sell_stocks_2.cpp
+++ test/buy_sell_stocks_2/test_buy_sell_stocks_2.cpp
@@ -7,7 +7,28 @@ using std::cout;
 using std::endl;
 
 #include "buy_sell_stocks_2_solution1.cpp"
+#include "buy_sell_stocks_2_solution2.cpp"
 
+
+TEST(buy_sell_stocks, test_1)
+{
+
+  std::vector<int> P     = {7,1,5,3,6,4};
+  constexpr int expected = 7;
+
+  EXPECT_EQ(expected, buy_sell_stocks_multiple_transactions_exp(P));
+  EXPECT_EQ(expected, buy_sell_stocks_multiple_transactions_lin(P));
+}
+
+TEST(buy_sell_stocks, test_all_increasing)
+{
+
+  std::vector<int> P     = {1,2,3,4,5};
+  constexpr int expected = 4;
+
+  EXPECT_EQ(expected, buy_sell_stocks_multiple_transactions_exp(P));
+  EXPECT_EQ(expected, buy_sell_stocks_multiple_transactions_lin(P));
+}
 
 
 TEST(buy_sell_stocks, test_all_negative)
@@ -16,7 +37,8 @@ TEST(buy_sell_stocks, test_all_negative)
   std::vector<int> P     = {-1, -2, -3, -4, -5, -6, -7, -8, -9};
   constexpr int expected = 0;
 
-  EXPECT_EQ(expected, buy_sell_stocks_multiple_transactions(P));
+  EXPECT_EQ(expected, buy_sell_stocks_multiple_transactions_exp(P));
+  EXPECT_EQ(expected, buy_sell_stocks_multiple_transactions_lin(P));
 }
 
 
