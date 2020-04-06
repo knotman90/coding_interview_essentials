7db19c8c11b7e24054ee4bf210ef0348e6d4bbaa
Davide Spataro <davide90.spataro@gmail.com>
2020-04-06 16:53:05 +0200

    add scaffolding code for median of sorted array problem

---
+++ test/median_sorted_arrays/test_median_sorted_arrays.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <bits/stdc++.h>
+
+using std::string;
+using std::swap;
+using std::cout;
+using std::endl;
+
+#include "median_sorted_arrays_solution1.cpp"
+#include "median_sorted_arrays_solution2.cpp"
+
+
+
+
+TEST(median_sorted_arrays, test1)
+{
+	
+	EXPECT_EQ(0,1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
+
