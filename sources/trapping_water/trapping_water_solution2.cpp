5a04c7eef09180699f49c32bc44cefda5cae33eb
Davide Spataro <davide90.spataro@gmail.com>
2020-04-12 22:53:16 +0200

    add two pointer solution to the trapping water problem

--- sources/trapping_water/trapping_water_solution2.cpp
+++ sources/trapping_water/trapping_water_solution2.cpp
@@ -4,15 +4,13 @@ std::vector<int> max_left_it(auto begin, auto end)
 
   int i    = 0;
   int cmax = *begin;
-  while(++begin != end)
+  while (++begin != end)
   {
     L[++i] = cmax;
-    cmax    = std::max(cmax, *begin);
+    cmax   = std::max(cmax, *begin);
   }
   return L;
 }
-
-
 
 int trapping_water_DP(const std::vector<int> &height)
 {
@@ -22,13 +20,13 @@ int trapping_water_DP(const std::vector<int> &height)
 
   int ans = 0;
   std::vector<int> L(max_left_it(height.begin(), height.end()));
-  //reversed input to calculate 
+  // reversed input to calculate
   std::vector<int> R(max_left_it(height.rbegin(), height.rend()));
   std::reverse(R.begin(), R.end());
-  
+
   for (size_t i = 0; i < height.size(); i++)
   {
-    ans += std::max(0,  std::min(R[i], L[i]) - height[i]);
+    ans += std::max(0, std::min(R[i], L[i]) - height[i]);
   }
 
   return ans;
--- sources/trapping_water/trapping_water_solution3.cpp
+++ sources/trapping_water/trapping_water_solution3.cpp
@@ -1,50 +1,26 @@
-/*std::vector<int> scan_inclusive(const std::vector<int> &h)
+int trapping_water_two_pointers(const std::vector<int>& H)
 {
+  int m_l, m_r;
+  m_l = m_r = 0;
 
-  std::vector<int> s(h.size());
-  s.back() = h.back();
-  for (int i = h.size() - 2; i >= 0; i--)
-  {
-    s[i] += s[i + 1];
-  }
-  return s;
-}
+  int l = 0;
+  int r = H.size() - 1;
 
-auto unwind(std::stack<int> &w, const int target)
-{
-  int ans   = 0;
-  int steps = 0;
-  while (!w.empty() && w.top() <= target)
-  {
-    ans += w.top();
-    w.pop();
-    ++steps;
-  }
-  return std::make_tuple(ans, steps);
-}
-
-int trapping_water_scan(const std::vector<int> &height)
-{
-  const size_t len = height.size();
-  if (len < 2)
-    return 0;
-
-
-  std::vector<int> maxR(max_right(height));
-  std::vector<int> s(scan_inclusive(height));
-
-  w.push(std::min(height[0], maxR[0]));
-  int m   = w.top();
   int ans = 0;
-  for (int i = 1; i < len; i++)
+  while (l < r)
   {
-    if (height[i] >= m)
+    if (H[l] <= H[r])
     {
-      auto [v, l] = unwind(w, m);
-      ans += (m * l) - v;
-      m = std::min(height[i], maxR[i]);
+      m_l = std::max(m_l, H[l]);
+      ans += m_l - H[l];
+      l++;
+    }
+    else
+    {
+      m_r = std::max(m_r, H[r]);
+      ans += m_r - H[r];
+      r--;
     }
   }
   return ans;
 }
-*/
