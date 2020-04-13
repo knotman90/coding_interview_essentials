29c3ba305c44a1ab64177c05296d1ebb2145f14e
Davide Spataro <davide90.spataro@gmail.com>
2020-04-13 23:18:15 +0200

    add linear time constant space solution to the majority element problem

--- test/majority_element/test_majority_element.cpp
+++ test/majority_element/test_majority_element.cpp
@@ -12,11 +12,48 @@ using std::endl;
 
 
 
-TEST(majority_element, test1)
+TEST(majority_element, test_empty_array)
 {
 	
-	EXPECT_EQ(0,1);
+	const std::vector<int> V = {};
+	constexpr int expected = -1;
+	EXPECT_EQ(expected, find_majority_element_linear(V));
 }
+
+
+TEST(majority_element, test_single_element)
+{
+	
+	const std::vector<int> V = {1};
+	constexpr int expected = 1;
+	EXPECT_EQ(expected, find_majority_element_linear(V));
+}
+
+TEST(majority_element, test_all_different)
+{
+	
+	const std::vector<int> V = {1,2,3,4,5,6,7,8};
+	constexpr int expected = -1;
+	EXPECT_EQ(expected, find_majority_element_linear(V));
+}
+
+TEST(majority_element, test_equal_times)
+{
+	
+	const std::vector<int> V = {1,1,2,2};
+	constexpr int expected = -1;
+	EXPECT_EQ(expected, find_majority_element_linear(V));
+}
+
+TEST(majority_element, test_equal_times_plus_one)
+{
+	
+	const std::vector<int> V = {2,1,1,2,2};
+	constexpr int expected = 2;
+	EXPECT_EQ(expected, find_majority_element_linear(V));
+}
+
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv); 
