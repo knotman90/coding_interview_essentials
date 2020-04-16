22b5865c9a62d70bdb81d301ac2e06933119ea36
Davide Spataro <davide90.spataro@gmail.com>
2020-04-16 22:28:08 +0200

    scaffolding code for number of islands

---
+++ test/number_islands/test_number_islands.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <bits/stdc++.h>
+
+using std::string;
+using std::swap;
+using std::cout;
+using std::endl;
+
+#include "number_islands_solution1.cpp"
+#include "number_islands_solution2.cpp"
+
+
+
+
+TEST(number_islands, test1)
+{
+	
+	EXPECT_EQ(0,1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
+
