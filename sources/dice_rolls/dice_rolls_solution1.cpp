ab6ad2d908563d1127022b604f8f011cbbc3e15f
Davide Spataro <davide90.spataro@gmail.com>
2020-04-27 13:00:41 +0200

    add dp solution

--- sources/dice_rolls/dice_rolls_solution1.cpp
+++ sources/dice_rolls/dice_rolls_solution1.cpp
@@ -1,22 +1,21 @@
-int num_rolls_to_target_bruteforce_helper(const int target,
-                                          const int dices,
-                                          const int f)
+int num_rolls_to_target_bruteforce(const int dices,
+                                   const int f,
+                                   const int target)
 {
   constexpr unsigned long MOD = 1e9 + 7;
   if (target < 0 || dices < 0)
     return 0;
 
   if (dices == 0)
-    return target == 0;
-
-  unsigned long ans = 0;
+    return target == 0 ? 1 : 0;
+    
+  cout << "Solving for: \tdices = "<<  dices << ",\ttarget = " << target <<endl;
+  // for each possible value this dice can assume
+  int ans = 0;
   for (int i = 1; i <= f; i++, ans %= MOD)
-    ans += num_rolls_to_target_bruteforce_helper(target - i, dices - 1, f);
-
+  {
+    
+    ans += num_rolls_to_target_bruteforce(dices - 1, f, target - i);
+  }
   return ans;
-}
-
-int num_rolls_to_target_bruteforce(int d, int f, int target)
-{
-  return num_rolls_to_target_bruteforce_helper(target, d, f);
 }
