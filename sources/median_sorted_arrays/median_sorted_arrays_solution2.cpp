cad2782b2a8ceb8b6ff30cf491f3292fc4137664
Davide Spataro <dspataro@degiro.nl>
2020-10-28 08:49:14 +0100

    add graph and merge arrays explanation

--- sources/median_sorted_arrays/median_sorted_arrays_solution2.cpp
+++ sources/median_sorted_arrays/median_sorted_arrays_solution2.cpp
@@ -1,6 +1,6 @@
 #include<cassert>
 template <typename T>
-std::vector<T> merge_sorted_arrays(const std::vector<T> &A,
+std::vector<T> mergeSortedArrays(const std::vector<T> &A,
                                    const std::vector<T> &B)
 {
   assert(std::is_sorted(std::begin(A), std::end(A)));
@@ -32,7 +32,7 @@ std::vector<T> merge_sorted_arrays(const std::vector<T> &A,
 double mediam_sorted_arrays_merge(const std::vector<int> &A,
                                   const std::vector<int> &B)
 {
-  std::vector<int> C = merge_sorted_arrays(A, B);
+  std::vector<int> C = mergeSortedArrays(A, B);
 
   const int mid = C.size() / 2;
   return (C.size() & 1) ? C[mid] : (C[mid] + C[mid + 1]) / 2.0;
