6e87bd01e9ece53b0e3960e02cfab92d216c8726
Davide Spataro <dspataro@degiro.nl>
2020-11-01 11:34:52 +0100

    add test folder

---
+++ test/kth_largest_in_stream/test_kth_largest_in_stream.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <bits/stdc++.h>
+
+using std::string;
+using std::swap;
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
+using std::vector;
+
+#include "kth_largest_in_stream_solution1.cpp"
+#include "kth_largest_in_stream_solution2.cpp"
+
+
+
+
+TEST(kth_largest_in_stream, test1)
+{
+	
+	EXPECT_EQ(0,1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
+
