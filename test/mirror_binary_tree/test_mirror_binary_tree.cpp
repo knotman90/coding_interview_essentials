09fe15cf41d9c307e1ca35e99d9e6bd8c6afbac8
Davide Spataro <dspataro@degiro.nl>
2020-11-01 11:37:58 +0100

    mirror binary tree first commit

---
+++ test/mirror_binary_tree/test_mirror_binary_tree.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <bits/stdc++.h>
+
+using std::string;
+using std::swap;
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
+using std::vector;
+
+#include "mirror_binary_tree_solution1.cpp"
+#include "mirror_binary_tree_solution2.cpp"
+
+
+
+
+TEST(mirror_binary_tree, test1)
+{
+	
+	EXPECT_EQ(0,1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
+
