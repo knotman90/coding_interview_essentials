3afc24478c974fea1c330d9cec33fc8c7efde178
Davide Spataro <dspataro@degiro.nl>
2020-11-07 20:34:40 +0100

    add draft for test. add images

---
+++ sources/mirror_binary_tree/mirror_binary_tree_solution1.cpp
@@ -0,0 +1,7 @@
+#include "binary_tree.h"
+
+template<typename T>
+Node<T>* mirror_binary_tree(const Node<T>*const node)
+{
+    return nullptr;
+}
--- test/mirror_binary_tree/test_mirror_binary_tree.cpp
+++ test/mirror_binary_tree/test_mirror_binary_tree.cpp
@@ -9,16 +9,65 @@ using std::begin;
 using std::end;
 using std::vector;
 
+#include "binary_tree.h"
 #include "mirror_binary_tree_solution1.cpp"
 #include "mirror_binary_tree_solution2.cpp"
 
 
-
-
-TEST(mirror_binary_tree, test1)
+template<typename T>
+Node<T>* treeFromList_helper( const std::vector<T>& payloads, const int pos)
 {
-	
-	EXPECT_EQ(0,1);
+    if(pos >= payloads.size())
+        return nullptr;
+    Node<T>* root = new Node<T>(payloads[pos]);
+    root->left = treeFromList_helper<T>(payloads, pos*2);
+    root->right = treeFromList_helper<T>(payloads, pos*2+1);
+    return root;
+}
+
+template<typename T>
+Node<T>* treeFromList( std::vector<T>& payloads){
+    payloads.insert(begin(payloads), T{});
+    return treeFromList_helper(payloads, 1);
+}
+
+template<typename T>
+bool equals(Node<T>* t1, Node<T>* t2)
+{
+    if(!t1 && !t2)
+        return true;
+    if((!t1 && t2) || (t1 && !t2))  
+        return false;
+    
+    return (t1->payload==t2->payload) && equals(t1->left, t2->left)
+    && equals(t1->right, t2->right);
+
+
+}
+
+TEST(mirror_binary_tree, test_empty)
+{
+     std::vector<int> values = {};
+     std::vector<int> values_mirror = {};
+
+    auto tree = treeFromList<int>(values);
+    auto tree_mirror = treeFromList<int>(values_mirror);
+    
+    auto res = mirror_binary_tree(tree);
+	EXPECT_TRUE(equals(tree_mirror,res ));
+}
+
+
+TEST(mirror_binary_tree, test_leaf)
+{
+     std::vector<int> values = {1};
+     std::vector<int> values_mirror = {1};
+
+    auto tree = treeFromList<int>(values);
+    auto tree_mirror = treeFromList<int>(values_mirror);
+    
+    auto res = mirror_binary_tree(tree);
+	EXPECT_TRUE(equals(tree_mirror,res ));
 }
 
 int main(int argc, char **argv) {
