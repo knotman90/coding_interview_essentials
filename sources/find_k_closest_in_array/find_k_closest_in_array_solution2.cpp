db64837a1cf8d2f823cf64a70301b043bd60a266
Davide Spataro <dspataro@degiro.nl>
2020-11-08 18:13:05 +0100

    add problem statement plus sorting solutions

---
+++ sources/find_k_closest_in_array/find_k_closest_in_array_solution2.cpp
@@ -0,0 +1,9 @@
+std::vector<int> kth_closest_in_array_partial_sorting(vector<int>& I, const int k, const int x)
+{
+    assert(I.size() >= k);
+    std::partial_sort(begin(I), begin(I)+k, end(I), [x](const auto y, const auto z){
+        return std::abs(x-y) < std::abs(x-z);
+    });
+    
+    return std::vector<int>(begin(I), begin(I)+k);
+}
---
+++ sources/find_k_closest_in_array/find_k_closest_in_array_solution3.cpp
@@ -0,0 +1,4 @@
+std::vector<int> kth_closest_in_array_binary_search_stl(vector<int>& I, const int k, const int x)
+{
+    
+}
