488395c90e5aebaabc26444d8e68e3e08fb7d0c2
Davide Spataro <dspataro@degiro.nl>
2020-11-08 22:45:11 +0100

    add binary search solution draft and sketch of the lower_bound solution

--- sources/find_k_closest_in_array/find_k_closest_in_array_solution1.cpp
+++ sources/find_k_closest_in_array/find_k_closest_in_array_solution1.cpp
@@ -1,9 +1,11 @@
-std::vector<int> kth_closest_in_array_sorting(vector<int>& I, const int k, const int x)
+std::vector<int> kth_closest_in_array_sorting(vector<int>& I,
+                                              const int k,
+                                              const int x)
 {
-    assert(I.size() >= k);
-    std::sort(begin(I), end(I), [x](const auto y, const auto z){
-        return std::abs(x-y) < std::abs(x-z);
-    });
-    
-    return std::vector<int>(begin(I), begin(I)+k);
+  assert(I.size() >= k);
+  std::sort(begin(I), end(I), [x](const auto y, const auto z) {
+    return std::abs(x - y) < std::abs(x - z);
+  });
+
+  return std::vector<int>(begin(I), begin(I) + k);
 }
--- sources/find_k_closest_in_array/find_k_closest_in_array_solution3.cpp
+++ sources/find_k_closest_in_array/find_k_closest_in_array_solution3.cpp
@@ -1,4 +1,23 @@
-std::vector<int> kth_closest_in_array_binary_search_stl(vector<int>& I, const int k, const int x)
+template<typename Iterator>
+ auto find_range(Iterator begin,
+                                            Iterator end,
+                                            Iterator closest){
+return std::make_tuple(0,0);            
+                                            }
+    std::vector<int> kth_closest_in_array_binary_search_lower_bound(
+        vector<int>& I, const int k, const int x)
 {
-    
+  auto closest = std::lower_bound(begin(I), end(I), x);
+  if (auto prec = std::prev(closest);
+      closest != begin(I) && closest != end(I) && (*closest > *prec))
+  {
+    closest = prec;
+  }
+  // if no element is larger than x
+  // then the closest to it is the largest in I
+  if (closest == end(I))
+    closest = std::prev(end(I));
+
+  auto [l, r] = find_range(begin(I), end(I), closest, k);
+  return std::vector<int>(begin(I) + l, begin(I) + r);
 }
