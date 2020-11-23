2865c737e7646f8858daf54cb5cf2c107484835b
Davide Spataro <dspataro@degiro.nl>
2020-11-23 16:20:21 +0100

    initial commit

---
+++ test/remove_duplicated_sorted_array_inplace/test_remove_duplicated_sorted_array_inplace.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <bits/stdc++.h>
+
+using std::string;
+using std::swap;
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
+using std::vector;
+
+#include "remove_duplicated_sorted_array_inplace_solution1.cpp"
+#include "remove_duplicated_sorted_array_inplace_solution2.cpp"
+
+
+
+
+TEST(remove_duplicated_sorted_array_inplace, test1)
+{
+	
+	EXPECT_EQ(0,1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
+
