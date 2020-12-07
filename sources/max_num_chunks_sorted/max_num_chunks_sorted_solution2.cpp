1202591e2d6267141889449b4fad859e3a0d4a57
Davide Spataro <dspataro@degiro.nl>
2020-12-07 10:24:42 +0100

    initial commit

---
+++ sources/max_num_chunks_sorted/max_num_chunks_sorted_solution2.cpp
@@ -0,0 +1,26 @@
+int maxChunksToSorted(std::vector<int>& arr)
+{
+  constexpr int INF = std::numeric_limits<int>::min();
+
+  if (arr.size() <= 0)
+    return 0;
+
+  std::set<int> N(std::begin(arr), std::end(arr));
+  int ans      = 0;
+  int curr_max = INF;
+  for (int i = 0; i < std::ssize(arr); i++)
+  {
+    N.erase(arr[i]);
+    const int new_max = std::max(curr_max, arr[i]);
+    if (N.size() > 0 && new_max >= *(N.begin()))
+    {
+      curr_max = new_max;
+    }
+    else
+    {
+      ans++;
+      curr_max = INF;
+    }
+  }
+  return ans;
+}
---
+++ test/max_num_chunks_sorted/test_max_num_chunks_sorted.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <bits/stdc++.h>
+
+using std::string;
+using std::swap;
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
+using std::vector;
+
+#include "max_num_chunks_sorted_solution1.cpp"
+#include "max_num_chunks_sorted_solution2.cpp"
+
+
+
+
+TEST(max_num_chunks_sorted, test1)
+{
+	
+	EXPECT_EQ(0,1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
+
