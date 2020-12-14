a736413274af98eb7198614259d8598998c774c8
Davide Spataro <dspataro@degiro.nl>
2020-12-14 11:03:28 +0100

    fix lineartime solution

--- sources/items_in_containers_amazon/items_in_containers_amazon_solution2.cpp
+++ sources/items_in_containers_amazon/items_in_containers_amazon_solution2.cpp
@@ -30,18 +30,36 @@ std::vector<int> prefix_sum_containers_items(const std::string& s)
     return cont_prefix_sum;
 }
 
-
+std::vector<int> find_closest_bars_right(const std::string& s)
+{
+    std::vector<int> ans(s.size());
+    int idx_last_bar = std::ssize(s)-1;
+    for(int i = std::ssize(s)-1 ; i >=0 ; i--)
+    {
+        if(s[i]=='|')
+            idx_last_bar = i;
+        ans[i] = idx_last_bar; 
+    }
+    return ans;
+}
 std::vector<int> items_in_containers_lineartime(const std::string& s,const std::vector<Query>& Q)
 {
    
-    const std::vector<int>& prefix_sum = prefix_sum_containers_items(s);
+    const std::vector<int>& prefix_sum_count_items = prefix_sum_containers_items(s);
+    const std::vector<int>& closest_bars_right = find_closest_bars_right(s);
     
     std::vector<int> ans;
     ans.reserve(Q.size());
-    for(const auto&[start,end] : Q)
+    for(const auto& [start,end] : Q)
     {
-        const auto& count_before_start = (start<=0) ? 0 : prefix_sum[start-1];
-        ans.push_back(prefix_sum[end] - count_before_start);
+        const auto& new_start = closest_bars_right[start];
+        if(new_start >= end){
+            ans.push_back(0);
+        }
+        else{    
+            const auto& count_before_start = (new_start<=0) ? 0 : prefix_sum_count_items[new_start];
+            ans.push_back(prefix_sum_count_items[end] - count_before_start);
+        }
     }
     return ans;
 }
--- test/items_in_containers_amazon/test_items_in_containers_amazon.cpp
+++ test/items_in_containers_amazon/test_items_in_containers_amazon.cpp
@@ -47,8 +47,8 @@ TEST(items_in_containers_amazon, test2)
 TEST(items_in_containers_amazon, test3)
 {
 	const std::string s = "|**|**|";
-    const std::vector<std::pair<int,int>> Q ={{1,5},{0,6}};
-    const std::vector<int> expected = {0,4};
+    const std::vector<std::pair<int,int>> Q ={{1,5},{0,5}};
+    const std::vector<int> expected = {0,2};
 
 	{
         EXPECT_EQ(expected,items_in_containers_naive(s,Q));
@@ -57,6 +57,37 @@ TEST(items_in_containers_amazon, test3)
         EXPECT_EQ(expected,items_in_containers_lineartime(s,Q));
     }
 }
+
+TEST(items_in_containers_amazon, test4)
+{
+	const std::string s = "|*|**|";
+    const std::vector<std::pair<int,int>> Q ={{3,5},{2,3},{0,4},{1,3}};
+    const std::vector<int> expected = {0,0,1,0};
+
+	{
+        EXPECT_EQ(expected,items_in_containers_naive(s,Q));
+    }
+    {
+        EXPECT_EQ(expected,items_in_containers_lineartime(s,Q));
+    }
+}
+
+TEST(items_in_containers_amazon, test5)
+{
+	const std::string s = "|**|*||**||*|";
+    const std::vector<std::pair<int,int>> Q ={{9,10} , {3,7} , {2,10} , {7,8} , {4,11}};
+    const std::vector<int> expected = {0, 1, 3, 0, 2};
+
+	{
+        EXPECT_EQ(expected,items_in_containers_naive(s,Q));
+    }
+    {
+        EXPECT_EQ(expected,items_in_containers_lineartime(s,Q));
+    }
+}
+
+
+
 
 
 std::string input_string_containers(const int stride_size, const int max_strides, const char sep = '|', const char item = '*')
@@ -86,14 +117,44 @@ auto generate_random_pair_in_range(const T s, const T e)
 }
 
 
+std::string to_string(const int& v)
+{
+    std::ostringstream ss;
+     ss<<v;
+    return ss.str();
+}
+
+std::string to_string(const std::pair<int,int>& q)
+{
+    std::ostringstream ss;
+     ss<<"(";
+        ss<<std::get<0>(q);
+        ss<<",";
+        ss<<std::get<1>(q);
+        ss<<") ";
+        return ss.str();
+}
+
+template<typename T>
+std::string to_string(const std::vector<T>& Q )
+{
+    std::ostringstream ss;
+    ss<<"[";
+    for(const auto& q: Q){
+        ss<<to_string(q);
+        ss<<", ";
+    }
+    ss<<"]";
+    return ss.str();
+}
 
 TEST(items_in_containers_amazon, test_random_all_solution_equal)
 {
-	const std::string s = input_string_containers(3, 10);
-    std::vector<std::pair<int,int>> Q(5);
+	const std::string s = input_string_containers(1000, 100);
+    std::vector<std::pair<int,int>> Q(1000);
     std::generate(
         Q.begin(), Q.end(), [&](){
-            return generate_random_pair_in_range<int>(0,s.size());
+            return generate_random_pair_in_range<int>(0,s.size()-1);
             }
     );
     
@@ -105,7 +166,9 @@ TEST(items_in_containers_amazon, test_random_all_solution_equal)
 
     ASSERT_TRUE(std::all_of(std::begin(answers), std::end(answers),
     [&](const auto& v){return v==answers.back();}
-    ));
+    )) << "error for "<<s<<std::endl << " Q= "<<to_string(Q)<<std::endl<<"answers = "<<to_string(answers);
+
+
     
 }
 
