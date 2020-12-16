7144f18a919ed59eb4ce7b6f07b20569d566a55f
Davide Spataro <dspataro@degiro.nl>
2020-12-16 20:33:07 +0100

    remove old cmake compatibility. add bruteforce description

--- sources/palindrome_partitioning2/palindrome_partitioning2_solution1.cpp
+++ sources/palindrome_partitioning2/palindrome_partitioning2_solution1.cpp
@@ -1,3 +1,18 @@
-int palindrome_partitioning2_bruteforce(const std::string s){
-	return -1; 
+#include "generate_combinations.h"
+
+bool is_palindrome(const std::string&s, const size_t start, const size_t end)
+{
+	assert(start <= end);
+	assert(end < s.length());
+
+	auto l = start, r = end;
+	while(l < r)
+		if(s[l--]!=s[r--])
+			return false;
+	return true;
+}
+
+int palindrome_partitioning2_bruteforce(const std::string s)
+{
+	
 }
