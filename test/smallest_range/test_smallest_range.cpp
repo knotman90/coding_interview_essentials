c746b0cbf422dc02f56ee9a1a0da2bb802bea34c
Davide Spataro <dspataro@degiro.nl>
2020-12-22 12:07:52 +0100

    add test for smallest_range 1

--- test/smallest_range/test_smallest_range.cpp
+++ test/smallest_range/test_smallest_range.cpp
@@ -1,28 +1,55 @@
-#include <gtest/gtest.h>
 #include <bits/stdc++.h>
+#include <gtest/gtest.h>
 
+using std::begin;
+using std::cout;
+using std::end;
+using std::endl;
 using std::string;
 using std::swap;
-using std::cout;
-using std::endl;
-using std::begin;
-using std::end;
 using std::vector;
 
 #include "smallest_range_solution1.cpp"
 #include "smallest_range_solution2.cpp"
 
-
-
-
-TEST(smallest_range, test1)
+TEST(smallest_range_1, test1)
 {
-	
-	EXPECT_EQ(0,1);
+  for (const std::tuple<std::vector<int>, int, int>& t : {
+           std::make_tuple(std::vector<int>({1}), 0, 0),
+           std::make_tuple(std::vector<int>({1, 3, 6}), 0, 5),
+           std::make_tuple(std::vector<int>({1, 3, 6}), 1, 3),
+           std::make_tuple(std::vector<int>({1, 3, 6}), 2, 1),
+           std::make_tuple(std::vector<int>({1, 3, 6}), 3, 0),
+           std::make_tuple(std::vector<int>({1, 3, 6}), 4, 0),
+           std::make_tuple(std::vector<int>({1, 3, 6}), 50, 0),
+           std::make_tuple(std::vector<int>({0, 10}), 2, 6),
+
+           std::make_tuple(std::vector<int>({1}), 0, 0),
+           std::make_tuple(std::vector<int>({6, 1, 3}), 0, 5),
+           std::make_tuple(std::vector<int>({6, 1, 3}), 1, 3),
+           std::make_tuple(std::vector<int>({6, 1, 3}), 2, 1),
+           std::make_tuple(std::vector<int>({6, 1, 3}), 3, 0),
+           std::make_tuple(std::vector<int>({6, 1, 3}), 4, 0),
+           std::make_tuple(std::vector<int>({6, 1, 3}), 50, 0),
+           std::make_tuple(std::vector<int>({0, 10}), 2, 6),
+
+           std::make_tuple(std::vector<int>({1}), 0, 0),
+           std::make_tuple(std::vector<int>({-6, -1, -3}), 0, 5),
+           std::make_tuple(std::vector<int>({-6, -1, -3}), 1, 3),
+           std::make_tuple(std::vector<int>({-6, -1, -3}), 2, 1),
+           std::make_tuple(std::vector<int>({-6, -1, -3}), 3, 0),
+           std::make_tuple(std::vector<int>({-6, -1, -3}), 4, 0),
+           std::make_tuple(std::vector<int>({-6, -1, -3}), 50, 0),
+           std::make_tuple(std::vector<int>({0, 10}), 2, 6),
+       })
+  {
+    const auto [I, K, expected] = t;
+    EXPECT_EQ(smallest_range_I(I, K), expected);
+  }
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv); 
-    return RUN_ALL_TESTS();
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
-
