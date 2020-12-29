7531184e07c416cf97b026c4d38afcbf37b06f52
Davide Spataro <dspataro@degiro.nl>
2020-12-29 19:58:25 +0100

    problem statement

---
+++ test/next_greater_element/test_next_greater_element.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <bits/stdc++.h>
+
+using std::string;
+using std::swap;
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
+using std::vector;
+
+#include "next_greater_element_solution1.cpp"
+#include "next_greater_element_solution2.cpp"
+
+
+
+
+TEST(next_greater_element, test1)
+{
+	
+	EXPECT_EQ(0,1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
+
