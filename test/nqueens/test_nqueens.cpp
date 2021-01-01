c94701c33239b4cf53e640662284a0021fb83afe
Davide Spataro <dspataro@degiro.nl>
2021-01-01 12:20:20 +0100

    add test for bruteforce

--- test/nqueens/test_nqueens.cpp
+++ test/nqueens/test_nqueens.cpp
@@ -15,11 +15,26 @@ using std::vector;
 
 
 
-TEST(nqueens, test1)
+TEST(nqueens, testFrom1To6)
 {
 	
-	EXPECT_EQ(0,1);
+    
+	EXPECT_EQ(1,nqueen_bruteforce(1).size());
+    
+    EXPECT_EQ(0,nqueen_bruteforce(2).size());
+
+    EXPECT_EQ(0,nqueen_bruteforce(3).size());
+
+    EXPECT_EQ(2,nqueen_bruteforce(4).size());
+
+    EXPECT_EQ(10,nqueen_bruteforce(5).size());
+
+    EXPECT_EQ(4,nqueen_bruteforce(6).size());
+
+    EXPECT_EQ(40,nqueen_bruteforce(7).size());
+
 }
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv); 
