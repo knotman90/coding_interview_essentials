f62eafc21e57012daae74f32f19390cb1ab7c17d
Davide Spataro <dspataro@degiro.nl>
2021-01-02 20:21:29 +0100

    review unique elements

--- sources/unique_elements/unique_elements_brute_force_final.cpp
+++ sources/unique_elements/unique_elements_brute_force_final.cpp
@@ -5,9 +5,10 @@ bool unique_elements_final(const std::string &s)
   if (s.size() > ALPH_SIZE)
     return false;
 
-  std::array<bool, ALPH_SIZE> F = {false};
-  for (size_t i = 0; i != s.size(); i++)
+  std::array<bool, ALPH_SIZE> F;
+  for (size_t i = 0; i < s.size(); i++)
   {
+    //index in F
     const int idx = s[i] - 'a';
     if (F[idx])
       return false;
