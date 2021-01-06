ea8081ee86f33eeded567a2dc20038c43ab95ff0
Davide Spataro <dspataro@degiro.nl>
2021-01-06 23:58:25 +0100

    solutions and tests

--- test/count_bits/test_count_bits.cpp
+++ test/count_bits/test_count_bits.cpp
@@ -11,14 +11,37 @@ using std::vector;
 
 #include "count_bits_solution1.cpp"
 #include "count_bits_solution2.cpp"
+#include "count_bits_solution3.cpp"
+#include "count_bits_solution4.cpp"
 
 
 
 
-TEST(count_bits, test1)
+TEST(count_bits, test10)
 {
 	
-	EXPECT_EQ(0,1);
+	constexpr int n = 16;
+    const std::vector<int> expected = {0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1};
+    {
+        auto ans = count_bits_bruteforce(n);
+        ASSERT_EQ(ans, expected);
+    }
+
+    {
+        auto ans = count_bits_DP(n);
+        ASSERT_EQ(ans, expected);
+    }
+
+        {
+        auto ans = count_bits_DP_short(n);
+        ASSERT_EQ(ans, expected);
+    }
+
+        {
+        auto ans = count_bits_DP_powers(n);
+        ASSERT_EQ(ans, expected);
+    }
+    
 }
 
 int main(int argc, char **argv) {
