c014fed2180e03fd831a62a386643dfc216a8498
Davide Spataro <dspataro@degiro.nl>
2021-01-13 17:19:15 +0100

    initial commit plus test plus solution

--- test/decode_string/test_decode_string.cpp
+++ test/decode_string/test_decode_string.cpp
@@ -1,28 +1,42 @@
-#include <gtest/gtest.h>
 #include <bits/stdc++.h>
+#include <gtest/gtest.h>
 
+using std::begin;
+using std::cout;
+using std::end;
+using std::endl;
 using std::string;
 using std::swap;
-using std::cout;
-using std::endl;
-using std::begin;
-using std::end;
 using std::vector;
 
 #include "decode_string_solution1.cpp"
 #include "decode_string_solution2.cpp"
 
-
-
-
 TEST(decode_string, test1)
 {
-	
-	EXPECT_EQ(0,1);
+  using TestInput = std::tuple<std::string, std::string>;
+
+  for (const auto [input, expected] :
+       {
+           /*TestInput("2[abc]3[ab]", "abcabcababab"),
+       TestInput("2[abc3[ab]]", "abcababababcababab"),
+       TestInput("2[abc]3[cd]ef", "abcabccdcdcdef"),*/
+       TestInput("start2[a3[b4[c2[d]e]f]g]end", "startabcddecddecddecddefbcddecddecddecddefbcddecddecddecddefgabcddecddecddecddefbcddecddecddecddefbcddecddecddecddefgend"),
+       TestInput("10[ciao]", "ciaociaociaociaociaociaociaociaociaociao"),
+
+
+       })
+  {
+
+      {
+          auto ans = decode_string(input);
+          EXPECT_EQ(expected, ans) << "failed for "<<input;
+      }
+  }
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv); 
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
-
