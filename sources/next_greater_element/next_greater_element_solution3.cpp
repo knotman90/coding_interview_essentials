08733d6f367dd2e4a392e3254e0ddaef98f588d0
Davide Spataro <dspataro@degiro.nl>
2021-01-16 16:34:43 +0100

    final touches

--- sources/next_greater_element/next_greater_element_solution3.cpp
+++ sources/next_greater_element/next_greater_element_solution3.cpp
@@ -3,19 +3,19 @@ next_greater_element_I_stack(const std::vector<int>& A,
                            const std::vector<int>& B)
 {
   std::vector<int> C(A.size());
-  std::stack<int> stack;
+  std::stack<int> N;
    std::unordered_map<int, int> C_val;
 
   for(int i = std::ssize(B)-1 ; i >=0 ; i--)
   {
-      while(!stack.empty() && B[i] > stack.top())
+      while(!N.empty() && B[i] > N.top())
       {
-          stack.pop(); //remove smaller elements than *it
+          N.pop(); //remove smaller elements than *it
       }
       //now the stack is either empty or contains an increasing sequence
-      if(!stack.empty())
-        C_val[B[i]] = stack.top();
-      stack.push(B[i]);
+      if(!N.empty())
+        C_val[B[i]] = N.top();
+      N.push(B[i]);
   }
   for (int i = 0; i < std::ssize(A); i++)
   {
