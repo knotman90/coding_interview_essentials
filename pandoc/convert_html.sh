2934792fad268fb24f93bfb99299e35b38f99feb
Davide Spataro <dspataro@degiro.nl>
2021-07-04 09:49:11 +0200

    fix title appearing at the very last line of the chapter

--- pandoc/convert_html.sh
+++ pandoc/convert_html.sh
@@ -27,36 +27,24 @@ MAINMD="$WORK_DIR/main.md"
 pandoc -f latex  --mathjax    -s --toc $SOURCE_ROOT/main.tex -o $MAINMD
 
 
-function sedeasy {
-  sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
-}
-
 # fix code blocks
 grep -e "\`\`\` {" $MAINMD  | while read -r line ; do
 echo " "
-    lineescaped=$(echo $line |  sed 's/[.[\*^$()+?{|}]/\\&/g') 
-  # echo "Processing $lineescaped"
     LABEL=$(echo $line | cut -d'{' -f2- | cut -d' ' -f1)
+    LABEL_LST=$(echo $LABEL |  sed 's/#list/#lst/g')
     LANG=$(echo $line | cut -d'{' -f2- | cut -d' ' -f2)
     CAPTION=$(echo $line | cut -d'{' -f2- | cut -d' ' -f4- | sed -n -e 's/^\(.*\)\(label=\)\(.*\)$/\1/p')
     
     LANGESC=$(echo $LANG | sed 's/+/p/g')
-    echo "~~~{$LABEL $LANG $CAPTION}"
     CAPTION_DOUBLE_QUOTES_REMOVED=$(echo $CAPTION | sed 's/\\"//g');
-    SUB="~~~{$LABEL $LANGESC $CAPTION_DOUBLE_QUOTES_REMOVED}"
-    echo "sed -i  's/'"$lineescaped"'/'"$SUB"'/1' $MAINMD"
-    #sed -i 's/``` {#.*language.*caption.*/'"$SUB"'/0' $MAINMD
-    #sed -i -r 's/'"$lineescaped"'/'"$SUB"'/g' $MAINMD
-    sed -i '0,/``` {#.*language.*caption.*/{s/``` {#.*language.*caption.*/'"$SUB"'/}'  $MAINMD
+    SUB="~~~{$LABEL_LST $LANGESC $CAPTION_DOUBLE_QUOTES_REMOVED}"
 
+
+    sed -i '0,/``` {#.*language.*caption.*/{s/``` {#.*language.*caption.*/'"$SUB"'/}'  $MAINMD
     
-    # your code goes here
-    #sedeasy "$lineescaped" "skjdhfksjdhfksjdhfksd" $MAINMD 
 done
 
 sed -i 's/```/~~~/g' $MAINMD
-
-grep '```' $MAINMD
 
 #cat $MAINMD  | grep "\`\`\`"
 
@@ -87,7 +75,7 @@ do
    echo "from $line to next chapter"
    echo "sed -n -E -e '/^# $line/,/^# /p' $MAINMD"
 
-   cat $MAINMD | sed -n -E -e "/^# $line/,/^# /p"  >> $FILENAME
+   cat $MAINMD | sed -n -E -e "/^# $line/,/^# /p"  | head -n -1 >> $FILENAME
    let "idx+=1" 
 
 done
