2b51dc0ee7105f462224bb975429576f7ec6c5c0
Davide Spataro <dspataro@degiro.nl>
2021-07-17 10:43:13 +0200

    add official css + some scrips

---
+++ pandoc/scripts.js
@@ -0,0 +1,11 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+inside body 
+  <script type="text/javascript">
+
+    $(document).ready(function(){
+     $(".example").prepend("<h3>Example<\h3>");
+    });
+    
+    </script>
