63c300e87959bc26eeeb35492e208b799b1bc8d4
Davide Spataro <dspataro@degiro.nl>
2021-08-23 12:57:30 +0200

    add chapter to fullbook

--- sources/gas_station/gas_station_solution1.cpp
+++ sources/gas_station/gas_station_solution1.cpp
@@ -17,7 +17,7 @@ bool can_complete_loop_from_station(const std::vector<int>& G,
     next_station = (curr_station + 1) % size;
     tank += G[curr_station];
   }
-  return tank >= C[curr_station];
+  return tank >= C[curr_station]; //make sure you can make the last leg
 }
 
 int solve_gast_station_bruteforce(const std::vector<int>& G,
@@ -26,7 +26,7 @@ int solve_gast_station_bruteforce(const std::vector<int>& G,
   const auto size = G.size();
   if(size == 1)
     return 0;
-    
+
   for (int i = 0; i < size; i++)
   {
     if (can_complete_loop_from_station(G, C, i))
