dd61bfb4d1e7268c02407152d7e766b972321050
Davide Spataro <dspataro@degiro.nl>
2021-08-23 12:55:27 +0200

    add solutions

---
+++ sources/gas_station/gas_station_solution1.cpp
@@ -0,0 +1,36 @@
+bool can_complete_loop_from_station(const std::vector<int>& G,
+                                    const std::vector<int>& C,
+                                    const int station)
+{
+  const auto size  = G.size(); 
+  int curr_station = station;
+  int next_station = (curr_station + 1) % size;
+  int tank         = G[curr_station];
+  while (next_station != station)
+  {
+    if (tank < C[curr_station])
+    {
+      return false;
+    }
+    tank -= C[curr_station];
+    curr_station = next_station;
+    next_station = (curr_station + 1) % size;
+    tank += G[curr_station];
+  }
+  return tank >= C[curr_station];
+}
+
+int solve_gast_station_bruteforce(const std::vector<int>& G,
+                                  const std::vector<int>& C)
+{
+  const auto size = G.size();
+  if(size == 1)
+    return 0;
+    
+  for (int i = 0; i < size; i++)
+  {
+    if (can_complete_loop_from_station(G, C, i))
+      return i;
+  }
+  return -1;
+}
