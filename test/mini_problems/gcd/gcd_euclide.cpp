16eb36fd061bfc34da630d7ad53b9bdabad35b16
Davide Spataro <dspataro@degiro.nl>
2021-09-12 21:16:54 +0200

    add solution euclide

--- test/mini_problems/gcd/gcd_euclide.cpp
+++ test/mini_problems/gcd/gcd_euclide.cpp
@@ -1,10 +1,8 @@
 int gcd_euclide(const int x, const int y)
 {
-    if((x%y==0))
-        return y;
-    
-    const auto reminder = x%y;
-    return gcd_euclide(y,reminder);
+  if ((x % y == 0))
+    return y;
 
+  const auto reminder = x % y;
+  return gcd_euclide(y, reminder);
 }
-
