31c09571176229860ba9c408e4799be6b845670f
Davide Spataro <dspataro@degiro.nl>
2021-09-12 21:16:54 +0200

    add solution euclide

--- test/mini_problems/gcd/gcd_bruteforce.cpp
+++ test/mini_problems/gcd/gcd_bruteforce.cpp
@@ -1,10 +1,10 @@
 int gcd_bruteforce(const int x, const int y)
 {
-    if(x < y )
-        return gcd_bruteforce(y,x);
-    
-    for(int i = y ; i > 1 ; i--)
-        if((x%i ==0)  && (y%i==0))
-            return i;
-    return 1;
+  if (x < y)
+    return gcd_bruteforce(y, x);
+
+  for (int i = y; i > 1; i--)
+    if ((x % i == 0) && (y % i == 0))
+      return i;
+  return 1;
 }
