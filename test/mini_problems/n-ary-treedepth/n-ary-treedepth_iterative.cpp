beaa335249d23311d456bd15c2f163aa48a11d83
Davide Spataro <dspataro@degiro.nl>
2021-10-31 10:51:14 +0100

    clang format

--- test/mini_problems/n-ary-treedepth/n-ary-treedepth_iterative.cpp
+++ test/mini_problems/n-ary-treedepth/n-ary-treedepth_iterative.cpp
@@ -1,22 +1,22 @@
-    int n_ary_treedepth_itearative(Node* root)
+int n_ary_treedepth_itearative(Node* root)
+{
+  if (!root)
+    return 0;
+
+  std::stack<std::pair<Node*, int>> nodes;
+  nodes.push({root, 1});
+
+  int ans = 1;
+  while (!nodes.empty())
+  {
+    auto [node, level] = nodes.top();
+    nodes.pop();
+    ans = std::max(ans, level);
+    for (const auto child : node->children)
     {
-        if(!root)
-            return 0;
-        
-        std::stack<std::pair<Node*, int>> nodes;
-        nodes.push({root,1});
-        
-        int ans = 1;
-        while(!nodes.empty())
-        {
-            auto [node, level] = nodes.top();
-            nodes.pop();
-            ans = std::max(ans, level);
-            for(const auto child : node->children)
-            {
-                nodes.push({child, level+1});
-            }            
-        }
-        
-        return ans;
+      nodes.push({child, level + 1});
     }
+  }
+
+  return ans;
+}
