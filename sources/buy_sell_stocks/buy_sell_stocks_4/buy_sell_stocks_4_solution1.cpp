474f667fd44360fa47d03845e35cca1d45956387
Davide Spataro <dspataro@degiro.nl>
2021-11-13 13:28:00 +0100

    disable variation 4

--- sources/buy_sell_stocks/buy_sell_stocks_4/buy_sell_stocks_4_solution1.cpp
+++ sources/buy_sell_stocks/buy_sell_stocks_4/buy_sell_stocks_4_solution1.cpp
@@ -1,24 +1,18 @@
-int maxProfit(const int K, std::vector<int>& P)
+int maxProfit(int K, vector<int>& P)
 {
   const int n = P.size();
   if (K == 0 || n <= 1)
     return 0;
+  std::vector<std::vector<int>> DP(n + 1, std::vector<int>(K + 1, 0));
 
-  std::vector<std::vector<int>> DP(n + 1, std::vector<int>(2, 0));
-
-  int curr_k = 1;
-  int prec_k = 0;
   for (int k = 1; k <= K; k++)
   {
-    int max_kminus1_to_left_of_i = DP[0][prec_k] - P[0];
+    int max_left = DP[0][k - 1] - P[0];
     for (int i = 1; i < n; i++)
     {
-      DP[i][curr_k] =
-          std::max(DP[i - 1][curr_k], max_kminus1_to_left_of_i + P[i]);
-      max_kminus1_to_left_of_i =
-          std::max(max_kminus1_to_left_of_i, DP[i][prec_k] - P[i]);
+      DP[i][k] = std::max(DP[i - 1][k], max_left + P[i]);
+      max_left = std::max(max_left, DP[i][k - 1] - P[i]);
     }
-    swap(curr_k,prec_k);
   }
-  return DP[n - 1][prec_k];
+  return DP[n - 1][K];
 }
