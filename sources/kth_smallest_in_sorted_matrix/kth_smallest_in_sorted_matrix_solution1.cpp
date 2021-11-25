cc61ac4b4b153332e90ff0462709f29f88265771
Davide Spataro <dspataro@degiro.nl>
2021-11-25 20:41:09 +0100

    add naive and nlogn solution

---
+++ sources/kth_smallest_in_sorted_matrix/kth_smallest_in_sorted_matrix_solution1.cpp
@@ -0,0 +1,12 @@
+using Matrix = std::vector<std::vector<int>>;
+int kth_smallest_in_sorted_matrix_naive(const Matrix& M, const size_t k){
+    assert(k > 0);
+    assert(M.size() && (k <= (M.size() * M[0].size())));
+
+    std::vector<int> M_linear;
+    for(const auto& row : M){
+        M_linear.insert(M_linear.end(),row.begin(), row.end());
+    }
+    std::partial_sort(M_linear.begin(), M_linear.begin()+k, M_linear.end());
+    return M_linear[k-1];
+}
